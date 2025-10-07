170b6d68d28df8f8bbf2e41ee98b0580b6106e29
kanashi <kanashi@me.com>
2025-10-07 23:43:34 +0200

    Added nil_ls

--- init.lua
+++ init.lua
@@ -53,7 +53,7 @@ require "oil".setup({
 })
 vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
-vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "lua_ls", "nil_ls" })
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.o.completeopt = "menu,menuone,noinsert,noselect"
 
