855abcc12ee6d3afd8034904c67659bcd6099c18
kanashi <kanashi@me.com>
2026-04-06 15:28:48 +0200

    Add undo directory configuration for Vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,18 @@
+local undodir = vim.fn.expand("~/.vim/undodir")
+if
+	vim.fn.isdirectory(undodir) == 0
+then
+	vim.fn.mkdir(undodir, "p")
+end
+
 vim.o.termguicolors = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.backup = false
+vim.o.writebackup = false
 vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.undodir = undodir
 vim.o.clipboard = "unnamedplus"
 vim.o.signcolumn = "yes"
 vim.o.cursorline = true
