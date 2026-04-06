4e34b9c8b459f2655321710e0674849f7ec6d724
kanashi <kanashi@me.com>
2026-04-06 15:52:11 +0200

    Refactor lua/config/options.lua to improve editor configuration and usability

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,40 @@
+vim.opt.termguicolors = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "100"
+vim.opt.showmatch = true
+vim.opt.cmdheight = 1
+vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.showmode = false
+vim.opt.pumheight = 10
+vim.opt.pumblend = 10
+vim.opt.winborder = "rounded"
+vim.opt.winblend = 0
+vim.opt.conceallevel = 0
+vim.opt.concealcursor = ""
+vim.opt.lazyredraw = true
+vim.opt.synmaxcol = 300
+vim.opt.fillchars = { eob = " "}
+
 local undodir = vim.fn.expand("~/.vim/undodir")
 if
 	vim.fn.isdirectory(undodir) == 0
@@ -5,26 +42,27 @@ then
 	vim.fn.mkdir(undodir, "p")
 end
 
-vim.o.termguicolors = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.swapfile = false
-vim.o.undofile = true
-vim.o.undodir = undodir
-vim.o.clipboard = "unnamedplus"
-vim.o.signcolumn = "yes"
-vim.o.cursorline = true
-vim.o.winborder = "rounded"
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = undodir
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 0
+vim.opt.autoread = true
+vim.opt.autowrite = false
 
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.smartindent = true
-vim.o.autoindent = true
-vim.o.wrap = false
-vim.o.scrolloff = 5
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backspace = "indent,eol,start"
+vim.opt.autochdir = false
+vim.opt.iskeyword:append("-")
+vim.opt.path:append("**")
+vim.opt.selection = "inclusive"
+vim.opt.nouse = "a"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.modifiable = true
+vim.opt.encoding = "UTF-8"
 
-vim.o.completeopt = "menu,menuone,noinsert,noselect"
 vim.cmd("set completeopt+=noselect")
